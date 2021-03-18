d838524d23bd5a66ad97ad56f66ebf4b8ab71f6b
refikbakalim <refikbakalim@gmail.com>
2021-03-18 23:29:40 +0300

    init project

---
+++ webapp.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request
+import random
+import time
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html") 
+
+@app.route('/get', methods=['GET'])
+def get():
+    sleep("GET")
+    return ""
+
+@app.route('/post', methods=['POST'])
+def post():
+    sleep("POST")
+    return ""
+
+@app.route('/put', methods=['PUT'])
+def put():
+    sleep("PUT")
+    return ""
+
+@app.route('/delete', methods=['DELETE'])
+def delete():
+    sleep("DELETE")
+    return ""
+
+def sleep(request):
+    response_time = random.randint(0,3000)
+    time.sleep(response_time/1000)
+    with open("logfile.txt", "a") as logfile:
+        logfile.write(request + "," + str(response_time) + "," + str(int(time.time())) + "\n")
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
