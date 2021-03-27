cf6366e6199074880f5f5df08bcb79b9c95b6a57
refikbakalim <refikbakalim@gmail.com>
2021-03-27 15:12:47 +0300

    Add watchdog

--- producer/main.py
+++ producer/main.py
@@ -1,24 +1,55 @@
 from kafka import KafkaProducer
 import os.path
-from time import sleep
+import time 
 from json import dumps
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
 
-index = 1
-length = 0
-path = "log/log.txt"
-
-while os.path.exists(path) == False:
-    sleep(1)
+index = 0
+path = "./log/log.txt"
+obspath = "./log"
 
 producer = KafkaProducer(bootstrap_servers=['kafka'],
                          value_serializer=lambda x: 
                          dumps(x).encode('utf-8'))
-while True:
+
+if __name__ == "__main__":
+    patterns = "*"
+    ignore_patterns = ""
+    ignore_directories = True
+    case_sensitive = True
+    my_event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
+
+def send(string):
     with open(path) as f:
+        global index
         for i, row in enumerate(f):
             if i == index: 
                 log_line = row.rstrip("\n")
                 index += 1
                 producer.send('LogTopic', value = log_line)
-                print(log_line) #send to kafka instead
-    sleep(1)
+                print(string + " " + log_line)
+
+def on_created(event):
+    send("Created")
+
+def on_modified(event):
+    send("Modified")
+
+if os.path.exists(path) == True:
+    send("Exists")
+
+my_event_handler.on_created = on_created
+my_event_handler.on_modified = on_modified
+
+go_recursively = True
+my_observer = Observer()
+my_observer.schedule(my_event_handler, obspath, recursive=go_recursively)
+
+my_observer.start()
+try:
+    while True:
+        time.sleep(2)
+except KeyboardInterrupt:
+    my_observer.stop()
+    my_observer.join()
