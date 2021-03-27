0848f953078991f9a74bb166eb5bd7a514dd6eca
unknown <refikbakalim@gmail.com>
2021-03-27 05:15:42 +0300

    update consumer

--- consumer/main.py
+++ consumer/main.py
@@ -1,3 +1,21 @@
 from kafka import KafkaConsumer
+from json import loads
+from pymongo import MongoClient
 
-print("hi kafka consumer")
+consumer = KafkaConsumer(
+    'LogTopic',
+     bootstrap_servers=['kafka'],
+     auto_offset_reset='earliest',
+     enable_auto_commit=True,
+     group_id='my-group',
+     value_deserializer=lambda x: loads(x.decode('utf-8')))
+
+#client = MongoClient('localhost:27017')
+client = MongoClient('mongodb://user:password@127.0.0.1:27017/datab')
+collection = client.log_database.log_collection
+
+while True:
+    for message in consumer:
+        message = message.value
+        collection.insert_one(message)
+        print('{} added to {}'.format(message, collection))
