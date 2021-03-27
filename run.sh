ab04fdebe5f43f10c9cf689a6e59c6a433a34319
unknown <refikbakalim@gmail.com>
2021-03-27 05:16:02 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
 docker-compose build && docker-compose up -d
-
-
-docker exec -it kafka bash -c '$KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper zookeeper:2181 --topic test -partitions 1 --replication-factor 1'
