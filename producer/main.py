5485818c541c583403014550ed1244af00a8024a
unknown <refikbakalim@gmail.com>
2021-03-27 05:14:37 +0300

    update producer

--- producer/main.py
+++ producer/main.py
@@ -1,3 +1,24 @@
 from kafka import KafkaProducer
+import os.path
+from time import sleep
+from json import dumps
 
-print("hi kafka producer")
+index = 1
+length = 0
+path = "log/log.txt"
+
+while os.path.exists(path) == False:
+    sleep(1)
+
+producer = KafkaProducer(bootstrap_servers=['kafka'],
+                         value_serializer=lambda x: 
+                         dumps(x).encode('utf-8'))
+while True:
+    with open(path) as f:
+        for i, row in enumerate(f):
+            if i == index: 
+                log_line = row.rstrip("\n")
+                index += 1
+                producer.send('LogTopic', value = log_line)
+                print(log_line) #send to kafka instead
+    sleep(1)
